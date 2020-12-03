ecc7d1f7ca5d2f84aee2b5ee8362ff93f61c801d
whitw <shkim7876@naver.com>
2020-12-03 15:22:29 +0900

    atomic operation test on simple_module.c

--- hello_module/simple_module.c
+++ hello_module/simple_module.c
@@ -1,10 +1,30 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/slab.h>
+
+int sum = 0;
+
+int test_thread(void* _arg)
+{
+	int* arg = (int*)_arg;
+	int value;
+	value = __sync_add_and_fetch(&sum, *arg);
+	printk("current sum = %d", value);
+	return 0;
+}
 
 int __init simple_module_init(void)
 {
+	int i;
+	int* arg;
 	printk("simple module\n");
+	for(i = 0; i < 4;i++){
+		arg = (int*)kmalloc(sizeof(int), GFP_KERNEL);
+		*arg = i;
+		kthread_run(&test_thread, (void*)arg, "test_thread");
+	}
 	return 0;
 }
 
